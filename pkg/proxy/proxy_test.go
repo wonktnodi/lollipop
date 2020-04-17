bbbac1ef026beaec5eed8626077927d07102d2ac
wonktondi <wonktondiq@gmail.com>
2020-04-17 17:10:49 +0800

    phase 2

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -1,11 +1,35 @@
 package proxy
 
 import (
+  "bytes"
   "context"
+  "fmt"
   "io"
   "strings"
+  "sync"
   "testing"
+  "time"
 )
+
+func TestEmptyMiddleware_ok(t *testing.T) {
+  expected := Response{}
+  result, err := EmptyMiddleware(dummyProxy(&expected))(context.Background(), &Request{})
+  if err != nil {
+    t.Errorf("The middleware propagated an unexpected error: %s\n", err.Error())
+  }
+  if result != &expected {
+    t.Errorf("The middleware returned an unexpected result: %v\n", result)
+  }
+}
+
+func TestEmptyMiddleware_multipleNext(t *testing.T) {
+  defer func() {
+    if r := recover(); r == nil {
+      t.Errorf("The code did not panic\n")
+    }
+  }()
+  EmptyMiddleware(NoopProxy, NoopProxy)
+}
 
 func explosiveProxy(t *testing.T) Proxy {
   return func(ctx context.Context, _ *Request) (*Response, error) {
@@ -34,4 +58,74 @@ func (d dummyReadCloser) Read(p []byte) (int, error) {
 
 func (d dummyReadCloser) Close() error {
   return nil
+}
+
+func TestWrapper(t *testing.T) {
+  expected := "supu"
+  ctx, cancel := context.WithCancel(context.Background())
+  defer cancel()
+
+  readCloser := &dummyRC{
+    r:  bytes.NewBufferString(expected),
+    mu: &sync.Mutex{},
+  }
+
+  r := NewReadCloserWrapper(ctx, readCloser)
+  var out bytes.Buffer
+  tot, err := out.ReadFrom(r)
+  if err != nil {
+    t.Errorf("Total bits read: %d. Err: %s", tot, err.Error())
+    return
+  }
+  if readCloser.IsClosed() {
+    t.Error("The subject shouldn't be closed yet")
+    return
+  }
+  if tot != 4 {
+    t.Errorf("Unexpected number of bits read: %d", tot)
+    return
+  }
+  if v := out.String(); v != expected {
+    t.Errorf("Unexpected content: %s", v)
+    return
+  }
+
+  cancel()
+  <-time.After(100 * time.Millisecond)
+  if !readCloser.IsClosed() {
+    t.Error("The subject should be already closed")
+    return
+  }
+}
+
+type dummyRC struct {
+  r      io.Reader
+  closed bool
+  mu     *sync.Mutex
+}
+
+func (d *dummyRC) Read(b []byte) (int, error) {
+  d.mu.Lock()
+  defer d.mu.Unlock()
+
+  if d.closed {
+    return -1, fmt.Errorf("Reading from a closed source")
+  }
+  return d.r.Read(b)
+}
+
+func (d *dummyRC) Close() error {
+  d.mu.Lock()
+  defer d.mu.Unlock()
+
+  d.closed = true
+  return nil
+}
+
+func (d *dummyRC) IsClosed() bool {
+  d.mu.Lock()
+  defer d.mu.Unlock()
+
+  res := d.closed
+  return res
 }
